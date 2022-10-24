cba0cbd998122d15aaf0a89c6e18fc589d8d2266
PT <20013003@mail.ru>
2022-10-25 00:02:33 +0300

    learn peremennie

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 // 1. Переменные
 
-let name = 'Roman'
+const firstName = 'Roman' //string
 const lastName = 'Troychenko'
-let age = 18
+let age = 18 //number
+const isProgrammer = true //boolean
 
 age = 20
 
 console.log(age)
+
+const _ = 'private'
+const $ = 'some value'
+
+//const if = 'make if' // err
+const withNum5 = 5
+//const 5withNum = 5 // err
+
