9e5e4c70de265d5eab10e8d2e7f7433fa1641edb
PT <20013003@mail.ru>
2022-10-25 00:24:05 +0300

    learn operator

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-// 1. Переменные
+// 1 Переменные
 
 const firstName = 'Roman' //string
-const lastName = 'Troychenko'
-let age = 18 //number
+//const lastName = 'Troychenko'
+// const age = 20 //number
 const isProgrammer = true //boolean
-
-age = 20
-
-console.log(age)
 
 const _ = 'private'
 const $ = 'some value'
@@ -16,3 +12,32 @@ const $ = 'some value'
 const withNum5 = 5
 //const 5withNum = 5 // err
 
+// 2 Мутирование
+//console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+
+
+// const LastName = prompt('Введите фамилию')
+// alert(firstName + ' ' + LastName)
+
+// 3 Операторы
+let currentYear = 2022
+const birthYear = 2001
+
+//const age = currentYear - birthYear
+
+const a = 10
+const b = 5
+
+let c = 32
+// c += a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(currentYear++)
+// console.log(--currentYear)
+
+// console.log(c)
+
+// 4 Типы данных
