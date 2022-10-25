0c35923d96c72d44d658a58b76bf5447168257e8
PT <20013003@mail.ru>
2022-10-25 23:02:47 +0300

    Learn operator prior

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // 1 Переменные
 
-const firstName = 'Roman' //string
-//const lastName = 'Troychenko'
-// const age = 20 //number
-const isProgrammer = true //boolean
+// const firstName = 'Roman' //string
+// //const lastName = 'Troychenko'
+// // const age = 20 //number
+// const isProgrammer = true //boolean
 
-const _ = 'private'
-const $ = 'some value'
+// const _ = 'private'
+// const $ = 'some value'
 
 //const if = 'make if' // err
-const withNum5 = 5
+// const withNum5 = 5
 //const 5withNum = 5 // err
 
 // 2 Мутирование
@@ -17,19 +17,19 @@ const withNum5 = 5
 
 
 // const LastName = prompt('Введите фамилию')
-// alert(firstName + ' ' + LastName)
+// // alert(firstName + ' ' + LastName)
 
-// 3 Операторы
-let currentYear = 2022
-const birthYear = 2001
+// // 3 Операторы
+// let currentYear = 2022
+// const birthYear = 2001
 
-//const age = currentYear - birthYear
+// //const age = currentYear - birthYear
 
-const a = 10
-const b = 5
+// const a = 10
+// const b = 5
 
-let c = 32
-// c += a
+// let c = 32
+// // c += a
 
 // console.log(a + b)
 // console.log(a - b)
@@ -41,3 +41,23 @@ let c = 32
 // console.log(c)
 
 // 4 Типы данных
+
+// const isProgrammer = true
+// const name = 'Roman'
+// const age = 20
+
+// // console.log(typeof isProgrammer)
+// // console.log(typeof name)
+// // console.log(typeof age)
+
+// // 5 Приоритет операторов
+
+// const fullAge = 21
+// const birthYear = 2001
+// const currentYear = 2022
+
+// const isFullAge = (currentYear - birthYear) >= fullAge
+// console.log(isFullAge)
+ // 6 Условные операторы
+
+const courseStatus = 'Pending' 
