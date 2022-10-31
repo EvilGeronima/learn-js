826c73adc75f3e10bd4683ca104fae17f541b16b
PT <20013003@mail.ru>
2022-10-31 21:39:13 +0300

    learn string

---
+++ js/03_string.js
@@ -0,0 +1,52 @@
+// 1 String
+// const age = 21
+// function getAge() {
+// 	return age
+// }
+
+// const output = 'Привет, меня зовут ' + name + ' и мой возраст ' + age + ' лет'
+// const output = `Привет, меня зовут ${name} и мой возраст ${getAge()} год`
+
+
+// const output = `
+// <div> This is div </div>
+// <p> This is p</p>
+// `
+
+// console.log(output);
+
+// const myName = 'Роман'
+// console.log(myName.length)
+// console.log(myName.toUpperCase())
+// console.log(myName.toLowerCase())
+// console.log(myName.charAt(2))
+// console.log(myName.indexOf('ан'))
+// console.log(myName.toLocaleLowerCase().startsWith('ром'))
+// console.log(myName.startsWith('Ром'))
+// console.log(myName.endsWith('ан'))
+// console.log(myName.repeat(6))
+
+// const string = '         Password  '
+// console.log(string.trim())
+// // console.log(string.trimLeft())
+// // console.log(string.trimRight())
+
+// function logPerson(s, name, age) {
+// 	if (age < 0) {
+// 		age = 'Еще не родился'
+// 	}
+// 	return `${s[0]}${name}${s[1]}${age}${s[2]}`
+
+// }
+
+// const personName = 'Роман'
+// const personAge = 22
+// const personName2 = 'Максим'
+// const personAge2 = -2
+
+// const output = logPerson`Имя: ${personName}, Возраст: ${personAge}!`
+// const output2 = logPerson`Имя: ${personName2}, Возраст: ${personAge2}!`
+
+// console.log(output)
+// console.log(output2)
+
