a008079617884673d4f7ec726598a6f41d227c0d
PT <20013003@mail.ru>
2022-11-01 16:30:12 +0300

    learn split, revers, join

--- js/script.js
+++ js/script.js
@@ -20,7 +20,12 @@ const fib = [1, 1, 2, 3, 5, 8, 13]
 // console.log(lastItem)
 // console.log(cars)
 
-console.log(cars);
-console.log(cars.reverse());
+// console.log(cars);
+// console.log(cars.reverse());
 
 // Задача 1
+
+const text = 'Привет, мы изучаем JS'
+const reverseText = text.split('').reverse().join('')
+
+console.log(reverseText)
