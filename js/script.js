2104500ef8109465b97718a52a94969bd75e32a6
PT <20013003@mail.ru>
2022-11-01 16:25:56 +0300

    learn method array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 // Массивы 
 
-const card = ['Мазда', 'Форд', 'БМВ', 'Мерседес']
+const cars = ['Мазда', 'Форд', 'БМВ', 'Мерседес']
 const fib = [1, 1, 2, 3, 5, 8, 13]
+
+// Function
+// function addItemToEnd {
+
+// }
+
+// Method
+// cars.push('Рено') // Добавляем в конец
+
+// cars.unshift('Волга') // Добавляем в начало
+
+// const firstItem = cars.shift() // Удаляет первый элемент
+// const lastItem = cars.pop() // Удаляет последний элемент
+
+// console.log(firstItem)
+// console.log(lastItem)
+// console.log(cars)
+
+console.log(cars);
+console.log(cars.reverse());
+
+// Задача 1
