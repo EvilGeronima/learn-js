6d34b9eeedd4c2a81f6f2e8d3fbb8a89ef67b7fd
PT <20013003@mail.ru>
2022-11-04 02:44:36 +0300

    learn base object

---
+++ js/05_array.js
@@ -0,0 +1,116 @@
+// Массивы
+
+const cars = ['Мазда', 'Форд', 'БМВ', 'Мерседес']
+const fib = [1, 1, 2, 3, 5, 8, 13]
+
+// Function
+// function addItemToEnd {
+
+// }
+
+// Method
+// cars.push('Рено') // Добавляем в конец
+
+// cars.unshift('Волга') // Добавляем в начало
+
+// const firstItem = cars.shift() // Удаляет первый элемент
+// const lastItem = cars.pop() // Удаляет последний элемент
+
+// console.log(firstItem)
+// console.log(lastItem)
+// console.log(cars)
+
+// console.log(cars);
+// console.log(cars.reverse());
+
+// const index = cars.indexOf('БМВ')
+// cars[index] = 'Порш'
+// console.log(cars)
+
+
+// Массив объектов
+
+// const people = [
+// 	{ name: 'Роман', budget: 4500 },
+// 	{ name: 'Евгений', budget: 6500 },
+// 	{ name: 'Дмитрий', budget: 1000 }
+// ]
+
+// let findedPerson
+
+// for (const person of people) {
+// 	if (person.budget === 6500) {
+// 		findedPerson = person
+// 	}
+// }
+// console.log(findedPerson)
+
+
+
+// const index = people.findIndex(function (person) {
+// 	return person.budget === 6500
+// })
+
+// const person = people.find(function (person) {
+// 	return person.budget === 6500
+// })
+
+// console.log(person)
+
+// const person = people.find(person => person.budget === 6500)
+
+// console.log(person)
+
+// console.log(cars.includes('Мазда'))
+
+// const upperCaseCars = cars.map(car => {
+// 	return car.toUpperCase()
+// })
+
+// console.log(upperCaseCars)
+// console.log(cars)
+
+
+// const pow2 = num => num ** 2
+// const sqrt = num => Math.sqrt(num)
+
+// const pow2Fib = fib.map(pow2).map(Math.sqrt)
+// const pow2Fib = fib.map(pow2)
+// const filteredNumvers = pow2Fib.filter(num => num > 20)
+// console.log(pow2Fib)
+// console.log(filteredNumvers)
+
+// console.log(pow2Fib)
+
+
+// const people = [
+// 	{ name: 'Роман', budget: 4200 },
+// 	{ name: 'Евгений', budget: 3500 },
+// 	{ name: 'Дмитрий', budget: 1700 }
+// ]
+
+// const allBudget = people.reduce((acc, person) => {
+// 	if (person.budget > 2000) {
+// 		acc += person.budget
+// 	}
+// 	return acc
+// }, 0)
+
+
+// const allBudget = people.filter(person => person.budget > 2000).reduce((acc, person) => {
+// 	acc += person.budget
+// 	return acc
+// }, 0)
+
+// console.log(allBudget);
+
+
+
+
+
+// Задача 1
+
+// const text = 'Привет, мы изучаем JS'
+// const reverseText = text.split('').reverse().join('')
+
+// console.log(reverseText)
--- js/script.js
+++ js/script.js
@@ -1,116 +1,55 @@
-// Массивы
+const person = {
+	myName: 'Roman',
+	age: 21,
+	isProgrammer: true,
+	languages: ['ru', 'en', 'de'],
+	// 'complex key': 'Complex Value',
+	// ['key_' + (1 + 3)]: 'Computed Key', // key_4
+	greet() {
+		console.log('greet from person');
+	}
+}
 
-const cars = ['Мазда', 'Форд', 'БМВ', 'Мерседес']
-const fib = [1, 1, 2, 3, 5, 8, 13]
+// console.log()
 
-// Function
-// function addItemToEnd {
+// console.log(person.name)
+// const ageKey = 'age'
+// console.log(person['age'])
+// console.log(person['complex key'])
+// console.log(person)
+// person.greet()
 
-// }
+// person.age++
+// person.languages.push('by')
+// console.log(person)
 
-// Method
-// cars.push('Рено') // Добавляем в конец
+//person['key_4'] = undefined
 
-// cars.unshift('Волга') // Добавляем в начало
+// delete person['key_4']
+// console.log(person)
+// console.log(person['key_4'])
 
-// const firstItem = cars.shift() // Удаляет первый элемент
-// const lastItem = cars.pop() // Удаляет последний элемент
+// const name = person.name
+// const age = person.age
+// const languages = person.languages
 
-// console.log(firstItem)
-// console.log(lastItem)
-// console.log(cars)
+// const { myName, age: personAge = 10, languages } = person
 
-// console.log(cars);
-// console.log(cars.reverse());
-
-// const index = cars.indexOf('БМВ')
-// cars[index] = 'Порш'
-// console.log(cars)
-
-
-// Массив объектов
-
-// const people = [
-// 	{ name: 'Роман', budget: 4500 },
-// 	{ name: 'Евгений', budget: 6500 },
-// 	{ name: 'Дмитрий', budget: 1000 }
-// ]
-
-// let findedPerson
-
-// for (const person of people) {
-// 	if (person.budget === 6500) {
-// 		findedPerson = person
-// 	}
-// }
-// console.log(findedPerson)
-
-
-
-// const index = people.findIndex(function (person) {
-// 	return person.budget === 6500
-// })
-
-// const person = people.find(function (person) {
-// 	return person.budget === 6500
-// })
+// console.log(myName, personAge, languages)
 
 // console.log(person)
 
-// const person = people.find(person => person.budget === 6500)
-
-// console.log(person)
-
-// console.log(cars.includes('Мазда'))
-
-// const upperCaseCars = cars.map(car => {
-// 	return car.toUpperCase()
-// })
-
-// console.log(upperCaseCars)
-// console.log(cars)
-
-
-// const pow2 = num => num ** 2
-// const sqrt = num => Math.sqrt(num)
-
-// const pow2Fib = fib.map(pow2).map(Math.sqrt)
-// const pow2Fib = fib.map(pow2)
-// const filteredNumvers = pow2Fib.filter(num => num > 20)
-// console.log(pow2Fib)
-// console.log(filteredNumvers)
-
-// console.log(pow2Fib)
-
-
-// const people = [
-// 	{ name: 'Роман', budget: 4200 },
-// 	{ name: 'Евгений', budget: 3500 },
-// 	{ name: 'Дмитрий', budget: 1700 }
-// ]
-
-// const allBudget = people.reduce((acc, person) => {
-// 	if (person.budget > 2000) {
-// 		acc += person.budget
+// for (let key in person) {
+// 	if (person.hasOwnProperty(key)) {
+// 		console.log('key:', key)
+// 		console.log('value:', person[key])
 // 	}
-// 	return acc
-// }, 0)
+// }
 
+// Object.keys(person).forEach((key) => {
+// 	console.log('key:', key)
+// 	console.log('value:', person[key])
+// })
+// console.log(keys)
 
-// const allBudget = people.filter(person => person.budget > 2000).reduce((acc, person) => {
-// 	acc += person.budget
-// 	return acc
-// }, 0)
-
-// console.log(allBudget);
-
-
-
-
-
-// Задача 1
-
-// const text = 'Привет, мы изучаем JS'
-// const reverseText = text.split('').reverse().join('')
-
-// console.log(reverseText)
+// Contex
