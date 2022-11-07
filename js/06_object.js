79552a7528bf13f22065d5dd6b863e7c496437e3
PT <20013003@mail.ru>
2022-11-07 19:08:15 +0300

    lets learn asynchrony

---
+++ js/06_object.js
@@ -0,0 +1,107 @@
+const person = {
+	myName: 'Roman',
+	age: 21,
+	isProgrammer: true,
+	languages: ['ru', 'en', 'de'],
+	// 'complex key': 'Complex Value',
+	// ['key_' + (1 + 3)]: 'Computed Key', // key_4
+	greet() {
+		console.log('greet from person');
+	},
+	info() {
+		console.log('this:', this)
+		console.log('Информация про человека по имени:', this.myName)
+	}
+}
+
+// console.log(person.name)
+// const ageKey = 'age'
+// console.log(person['age'])
+// console.log(person['complex key'])
+// console.log(person)
+// person.greet()
+
+// person.age++
+// person.languages.push('by')
+// console.log(person)
+
+// person['key_4'] = undefined
+
+// delete person['key_4']
+// console.log(person)
+// console.log(person['key_4'])
+
+// const name = person.name
+// const age = person.age
+// const languages = person.languages
+
+// const { myName, age: personAge = 10, languages } = person
+
+// console.log(myName, personAge, languages)
+
+// console.log(person)
+
+// for (let key in person) {
+// 	if (person.hasOwnProperty(key)) {
+// 		console.log('key:', key)
+// 		console.log('value:', person[key])
+// 	}
+// }
+
+// Object.keys(person).forEach(key => {
+// 	console.log('key:', key)
+// 	console.log('value:', person[key])
+// })
+// console.log(keys)
+
+
+
+// console.log()
+
+
+
+// Context
+
+
+// person.info()
+
+const logger = {
+	keys() {
+		console.log('Object Keys:', Object.keys(this))
+	},
+	keysAndValues() {
+		Object.keys(this).forEach(key => {
+			console.log(`${key}: ${this[key]}`)
+		})
+
+		// const self = this
+		// Object.keys(this).forEach(function (key) {
+		// 	console.log(`${key}: ${this[key]}`)
+		// }.bind(this))
+	},
+	withParams(top = false, between = false, bottom = false) {
+		if (top) {
+			console.log('------- Start -------')
+		}
+		Object.keys(this).forEach((key, index, array) => {
+			console.log(`${key}: ${this[key]}`)
+			if (between && index !== array.length - 1) {
+				console.log('------------------')
+			}
+		})
+
+		if (bottom) {
+			console.log('------- End -------')
+		}
+	}
+}
+
+ const bound = logger.keys.bind(person)
+ bound()
+
+logger.keys.call(person)
+
+logger.keysAndValues.call(person)
+
+logger.withParams.call(person, true, true, true)
+logger.withParams.apply(person, [true, true, true])
--- js/script.js
+++ js/script.js
@@ -1,105 +1,14 @@
-const person = {
-	myName: 'Roman',
-	age: 21,
-	isProgrammer: true,
-	languages: ['ru', 'en', 'de'],
-	// 'complex key': 'Complex Value',
-	// ['key_' + (1 + 3)]: 'Computed Key', // key_4
-	greet() {
-		console.log('greet from person');
-	},
-	info() {
-		console.log('this:', this)
-		console.log('Информация про человека по имени:', this.myName)
-	}
-}
+// Event Loop
 
-// console.log(person.name)
-// const ageKey = 'age'
-// console.log(person['age'])
-// console.log(person['complex key'])
-// console.log(person)
-// person.greet()
+// const timeout = setTimeout(() => {
+// 	console.log('After timeout');
+// }, 2500)
 
-// person.age++
-// person.languages.push('by')
-// console.log(person)
+// clearTimeout(timeout)
 
-//person['key_4'] = undefined
+// const interval = setInterval(() => {
+// 	console.log('After timeout');
+// }, 1000)
 
-// delete person['key_4']
-// console.log(person)
-// console.log(person['key_4'])
+// clearTimeout(interval)
 
-// const name = person.name
-// const age = person.age
-// const languages = person.languages
-
-// const { myName, age: personAge = 10, languages } = person
-
-// console.log(myName, personAge, languages)
-
-// console.log(person)
-
-// for (let key in person) {
-// 	if (person.hasOwnProperty(key)) {
-// 		console.log('key:', key)
-// 		console.log('value:', person[key])
-// 	}
-// }
-
-// Object.keys(person).forEach(key => {
-// 	console.log('key:', key)
-// 	console.log('value:', person[key])
-// })
-// console.log(keys)
-
-
-
-// console.log()
-
-
-
-// Context
-// person.info()
-
-const logger = {
-	keys() {
-		console.log('Object Keys:', Object.keys(this))
-	},
-	keysAndValues() {
-		Object.keys(this).forEach(key => {
-			console.log(`${key}: ${this[key]}`)
-		})
-
-		// const self = this
-		// Object.keys(this).forEach(function (key) {
-		// 	console.log(`${key}: ${this[key]}`)
-		// }.bind(this))
-	},
-	withParams(top = false, between = false, bottom = false) {
-		if (top) {
-			console.log('------- Start -------')
-		}
-		Object.keys(this).forEach((key, index, array) => {
-			console.log(`${key}: ${this[key]}`)
-			if (between && index !== array.length - 1) {
-				console.log('------------------')
-			}
-		})
-
-		if (bottom) {
-			console.log('------- End -------')
-		}
-	}
-}
-
-//  const bound = logger.keys.bind(person)
-//  bound()
-
-// logger.keys.call(person)
-
-// logger.keysAndValues.call(person)
-
-logger.withParams.call(person, true, true, true)
-logger.withParams.apply(person, [true, true, true])
