88597e3c1ec5a7a7c6dcc1708096ae02e9975638
PT <20013003@mail.ru>
2022-11-07 19:36:57 +0300

    end learn async

--- js/script.js
+++ js/script.js
@@ -24,15 +24,17 @@ const delay = (wait = 1000) => {
 	const promise = new Promise((resolve, reject) => {
 		setTimeout(() => {
 			resolve()
+
+			//reject('Что то пошло не так. Повторите попытку')
 		}, wait)
 	})
 	return promise
 }
 
-delay(2500)
-	.then(() => console.log('After 2 seconds'))
-	.catch(err => console.error(err))
-	.finally(() => console.log('Finally'))
+// delay(2500)
+// 	.then(() => console.log('After 2 seconds'))
+// 	.catch(err => console.error(err))
+// 	.finally(() => console.log('Finally'))
 
 
 const getData = () => new Promise(resolve => resolve([
@@ -42,3 +44,18 @@ const getData = () => new Promise(resolve => resolve([
 getData().then(data => console.log(data))
 
 //колбэк объяснение
+
+async function asyncExample() { // уточнить по поводу асинк, как писать ее со стрелочной функции
+
+	try {
+		await delay(3000)
+		const data = await getData()
+		console.log('Data', data)
+	} catch (e) {
+		console.log(e);
+	} finally {
+		console.log('Finally');
+	}
+}
+
+asyncExample()
