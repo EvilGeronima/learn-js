28815fd25b7b0b01c8144a7fe02a98e3d7c8742e
PT <20013003@mail.ru>
2022-11-10 20:22:57 +0300

    learn style js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,14 @@
 
 const heading = document.getElementById('hello')
 
-console.dir(heading)
+console.dir(heading.textContent)
+
+heading.textContent = 'Выучил JS?'
+
+heading.style.color = 'yellow'
+
+heading.style.textAlign = 'center'
+
+heading.style.backgroundColor = 'black'
+
+heading.style.padding = '2rem'
